93f22f5190caf52a2c48bc9617e0e71235d74d0c
Scuti Code <scutiii.code@gmail.com>
2026-08-09 01:32:51 +0800

    TODO: FIX THE ROLE_OPTION COMMAND IN MODERATION COG

--- cogs/moderation/migrations/001_initial.sql
+++ cogs/moderation/migrations/001_initial.sql
@@ -1,3 +1,6 @@
+-- -----------------------------------------------------------------------------
+-- Other Moderation 
+-- -----------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS cases(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     case_number INTEGER NOT NULL,
@@ -18,3 +21,36 @@ ON cases(target_id);
 
 CREATE INDEX IF NOT EXISTS idx_cases_action
 ON cases(action);
+
+-- -----------------------------------------------------------------------------
+-- Role option messages
+-- -----------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS role_options(
+    message_id INTEGER PRIMARY KEY,
+    channel_id INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS role_option_roles(
+    message_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    emoji TEXT NOT NULL,
+
+    PRIMARY KEY(
+        message_id,
+        role_id
+    ),
+
+    UNIQUE(
+        message_id,
+        emoji
+    ),
+
+    FOREIGN KEY(
+        message_id
+    )
+    REFERENCES role_options(message_id)
+    ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_role_option_roles_emoji
+ON role_option_roles(message_id, emoji);
