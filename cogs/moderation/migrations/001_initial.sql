7277746ad05a813d02e97ae599687fb2ab8489fd
Scuti Code <scutiii.code@gmail.com>
2026-08-09 21:44:47 +0800

    removed roleoption

--- cogs/moderation/migrations/001_initial.sql
+++ cogs/moderation/migrations/001_initial.sql
@@ -21,36 +21,3 @@ ON cases(target_id);
 
 CREATE INDEX IF NOT EXISTS idx_cases_action
 ON cases(action);
-
--- -----------------------------------------------------------------------------
--- Role option messages
--- -----------------------------------------------------------------------------
-CREATE TABLE IF NOT EXISTS role_options(
-    message_id INTEGER PRIMARY KEY,
-    channel_id INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS role_option_roles(
-    message_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    emoji TEXT NOT NULL,
-
-    PRIMARY KEY(
-        message_id,
-        role_id
-    ),
-
-    UNIQUE(
-        message_id,
-        emoji
-    ),
-
-    FOREIGN KEY(
-        message_id
-    )
-    REFERENCES role_options(message_id)
-    ON DELETE CASCADE
-);
-
-CREATE INDEX IF NOT EXISTS idx_role_option_roles_emoji
-ON role_option_roles(message_id, emoji);
